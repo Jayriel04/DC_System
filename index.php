5d103a6ccc44da420d1afc55f6137e441fa3eff7
Jayriel04 <senilla.jayriel.mcc@gmail.com>
2025-09-27 21:42:25 +0800

    return

--- index.php
+++ index.php
@@ -7,66 +7,6 @@ include('includes/dbconnection.php');
 <html>
 
 <head>
-<<<<<<< HEAD
-<title>JF Dental Care</title>
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--bootstrap-->
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
-<!--coustom css-->
-<link href="css/style.css" rel="stylesheet" type="text/css"/>
-<!--script-->
-<script src="js/jquery-1.11.0.min.js"></script>
-<!-- js -->
-<script src="js/bootstrap.js"></script>
-<!-- /js -->
-<!--fonts-->
-<link href='//fonts.googleapis.com/css?family=Open+Sans:300,300italic,400italic,400,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
-<!--/fonts-->
-<!--hover-girds-->
-<link rel="stylesheet" type="text/css" href="css/default.css" />
-<link rel="stylesheet" type="text/css" href="css/component.css" />
-<script src="js/modernizr.custom.js"></script>
-<!--/hover-grids-->
-<script type="text/javascript" src="js/move-top.js"></script>
-<script type="text/javascript" src="js/easing.js"></script>
-<!--script-->
-<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){		
-					event.preventDefault();
-					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
-				});
-			}); 
-</script>
-<!--/script-->
-</head>
-	<body>
-<?php include_once('includes/header.php');?>
-
-  <div class="container">
-  <script src="js/responsiveslides.min.js"></script>
- <script>
-    $(function () {
-      $("#slider").responsiveSlides({
-        auto: true,
-        nav: true,
-        speed: 500,
-        namespace: "callbacks",
-        pager: true,
-      });
-    });
-  </script>
-<div class="slider">
-       <div class="callbacks_container">
-        <ul class="rslides" id="slider">
-         <li>     
-          <h3>WHERE YOUR SMILE GET BRIGHTNESS YOUR DAY!</h3> 
-          <p class="page1">A healthy smile is a reflection of a happy life. Let us help you shine your brightest. </p>                 
-          <div class="readmore">
-          <a href="user/create_account.php">Book appointment<i class="glyphicon glyphicon-menu-right"> </i></a>
-          </div>
-         </li>
-=======
   <title>JF Dental Care</title>
   <script
     type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
@@ -102,7 +42,6 @@ include('includes/dbconnection.php');
   </script>
   <!--/script-->
 </head>
->>>>>>> b7d183ea41f085f959dd6a501718258fcd9c2571
 
 <body>
   <?php include_once('includes/header.php'); ?>
